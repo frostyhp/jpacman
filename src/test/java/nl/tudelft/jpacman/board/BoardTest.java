faafeec0b9b90ad400c593f7b1b8fdd3a059faed
frostyhp <46947769+frostyhp@users.noreply.github.com>
2022-11-07 11:35:04 +0700

    Update BoardTest.java

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -1,55 +1,41 @@
 package nl.tudelft.jpacman.board;
+
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
+
 /**
  * Basic implementation of unit.
  *
  * @author Jeroen Roosen 
  */
 public class BoardTest {
-
     private Square[][] grid;
     private Board board;
 
     private final Square square = new BasicSquare();
-    
+
     /**
-    * isBoardValid
-    */
+     * basicSquare.
+     */
     @Test
-    void isBoardValid (){
-        grid = new Square[][]{
-            { square, square}
-        };
+    void basicSquare() {
+        grid = new Square[][]{{ square, square}};
         board = new Board(grid);
         assertThat(board.invariant()).isEqualTo(true);
     }
-    /**
-    * testSquareAt
-    */
-    @Test
-    void testSquareAt() {
-        grid = new Square[][]{
-            { square, null}
-        };
-        board = new Board(grid);
-        assertThat(board.squareAt( 0, 0)).isEqualTo(square);
 
-    }
     /**
-    * testSquareAt2
-    */
+     * testSquareAt2.
+     */
     @Test
     void testSquareAt2() {
         grid = new Square[][]{
-            {square,square}
+            {square, square}
         };
         board = new Board(grid);
-        assertThat(board.squareAt( 0, 0)).isEqualTo(square);
-
+        assertThat(board.squareAt(0, 0)).isEqualTo(square);
     }
+
+
 }

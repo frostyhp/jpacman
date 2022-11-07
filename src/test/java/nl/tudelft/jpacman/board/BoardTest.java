059f5d5d49d4ba3d7373d9a34134e37638219391
frostyhp <46947769+frostyhp@users.noreply.github.com>
2022-11-07 11:29:29 +0700

    Update BoardTest.java

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -5,13 +5,21 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
+/**
+ * Basic implementation of unit.
+ *
+ * @author Jeroen Roosen 
+ */
 public class BoardTest {
 
     private Square[][] grid;
     private Board board;
 
     private final Square square = new BasicSquare();
-
+    
+    /**
+    * isBoardValid
+    */
     @Test
     void isBoardValid (){
         grid = new Square[][]{
@@ -20,6 +28,9 @@ public class BoardTest {
         board = new Board(grid);
         assertThat(board.invariant()).isEqualTo(true);
     }
+    /**
+    * testSquareAt
+    */
     @Test
     void testSquareAt() {
         grid = new Square[][]{
@@ -29,6 +40,9 @@ public class BoardTest {
         assertThat(board.squareAt(0, 0)).isEqualTo(square);
 
     }
+    /**
+    * testSquareAt2
+    */
     @Test
     void testSquareAt2() {
         grid = new Square[][]{
